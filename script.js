df9ec37448adb61b130af267a93dc13b7f52afde
fatih <fsenyigit8@gmail.com>
2024-01-24 09:00:02 +0300

    error is fixed

--- script.js
+++ script.js
@@ -3,8 +3,11 @@
 
 const year = +prompt("Please enter a year");
 
-if(year%4==0) {
-    if((year%100 == 0)&&(year%400==0)) {
+if(year%4==0) { 
+    if((year%4==0 && year%100 !=0)) {
+        alert(`${year} is a leap year.`);
+    }
+    else if((year%100 == 0)&&(year%400==0)) {
         alert(`${year} is a leap year.`);
     } else {
         alert(`${year} is not a leap year.`);
